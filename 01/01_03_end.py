b3519d6207fd39ff3efd4891f6acfedb036e20b4
jk117463 <92452727+jk117463@users.noreply.github.com>
2023-09-27 12:38:14 +0200

    object created

--- 01/01_03_end.py
+++ 01/01_03_end.py
@@ -1,4 +1,3 @@
-
 import random
 from string import punctuation
 from collections import defaultdict
@@ -11,17 +10,16 @@ class MarkovChain:
     def _tokenize(self, text):
         return (
             text.translate(str.maketrans("", "", punctuation + "1234567890"))
-            .replace("\n", " ")
-            .split(" ")
+                .replace("\n", " ")
+                .split(" ")
         )
 
     def train(self, text):
         tokens = self._tokenize(text)
         for i, token in enumerate(tokens):
-            if (len(tokens) - 1)  == i:
+            if (len(tokens) - 1) == i:
                 break
             self.graph[token].append(tokens[i + 1])
-               
 
     def generate(self, prompt, length=10):
         # get the lask token from the prompt
@@ -34,7 +32,18 @@ class MarkovChain:
             if not options:
                 continue
             # use random.choice method to pick a current option
-            
+            current = random.choice(options)
             # add the random choice to the output string
-    
+            output += f" {current}"
         return output
+
+# Create an instance of the MarkovChain class
+mc = MarkovChain()
+
+# Train the Markov chain with some text data
+text_data = "This is a simple example of a Markov chain. Markov chains are used in various applications."
+mc.train(text_data)
+
+# Generate text using a prompt
+generated_text = mc.generate("This is", length=20)
+print(generated_text)
